7fdd131b10409584bbafd4be86a934b636612bc0
lgdtd3 <78389775+lgdtd3@users.noreply.github.com>
2023-05-31 22:05:32 +0800

    Create fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    print('时间到！')
+
+if __name__ == '__main__':
+    countdown(25 * 60) # 专注时长为25分钟
